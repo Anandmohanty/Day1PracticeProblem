91691d18876b6b5d19b3368a9bc023778879fe5d
Anandmohanty <mohantyanand3@gmail.com>
2023-06-28 22:41:36 +0530

    [Anand] - Welcome to Practice

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day1PracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Practice Problem ");
+        }
+    }
+}
